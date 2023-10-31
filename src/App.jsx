b9ddfc910ae65e75823813d18b2e16d54294c716
Gabriela <gdemelo.mendes@gmail.com>
2023-10-31 11:41:06 -0300

    Added some more styles and default prop values to ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import { styled } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-	font-size: 30px;
-	font-weight: 600;
-	background-color: yellow;
-`;
-
-const Button = styled.button`
-	font-size: 1.4rem;
-	padding: 1.2rem 1.6rem;
-	font-weight: 500;
-	border: none;
-	border-radius: 7px;
-	background-color: purple;
-	color: white;
-	cursor: pointer;
-`;
-
-const Input = styled.input`
-	border: 1ox solid #ddd;
-	border-radius: 5px;
-	padding: 0.8rem 1.2rem;
-`;
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-	background-color: orangered;
 	padding: 20px;
 `;
 
@@ -34,9 +14,27 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>The Wild Oasis</H1>
-				<Button>Check in</Button>
-				<Input type="number" placeholder="Number of guests"></Input>
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">The Wild Oasis</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button>
+								Check in
+							</Button>
+							<Button variation="secondary" size="small">
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row>
+						<Heading as="h3">Form</Heading>
+						<form action="">
+							<Input type="number" placeholder="Number of guests"></Input>
+							<Input type="number" placeholder="Number of guests"></Input>
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
