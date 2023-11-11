2a3b4b5e9bed6f1febe6ca7489b419c8a914371e
Gabriela <gdemelo.mendes@gmail.com>
2023-11-11 18:14:28 -0300

    Implemented file upload to supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -30,7 +30,10 @@ function CreateCabinForm() {
 	});
 
 	function onSubmit(data) {
-		mutate(data);
+		mutate({
+			...data,
+			image: data.image[0],
+		});
 	}
 
 	function onError(errors) {
@@ -111,7 +114,14 @@ function CreateCabinForm() {
 			</FormRow>
 
 			<FormRow label="Cabin photo">
-				<FileInput id="image" accept="image/*" disabled={isCreating} />
+				<FileInput
+					id="image"
+					accept="image/*"
+					disabled={isCreating}
+					{...register("image", {
+						required: "This field is required",
+					})}
+				/>
 			</FormRow>
 
 			<FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
 	const { data, error } = await supabase.from("cabins").select("*");
@@ -12,9 +12,16 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+		"/",
+		""
+	);
+	const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+	// Create cabin
 	const { data, error } = await supabase
 		.from("cabins")
-		.insert([newCabin])
+		.insert([{ ...newCabin, image: imagePath }])
 		.select();
 
 	if (error) {
@@ -22,6 +29,19 @@ export async function createCabin(newCabin) {
 		throw new Error("Cabin could not be created");
 	}
 
+	// Upload image
+	const { error: storageError } = await supabase.storage
+		.from("cabin-images")
+		.upload(imageName, newCabin.image);
+
+	// Delete cabin IF there was an error uploading image
+	if (storageError) {
+		await supabase.from("cabins").delete().eq("id", data[0].id);
+		console.error(storageError);
+		throw new Error(
+			"Cabin image could not be uploaded and the cabin was not created"
+		);
+	}
 	return data;
 }
 
