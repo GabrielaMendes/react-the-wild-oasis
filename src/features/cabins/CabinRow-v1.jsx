c1b921f5a9f2fffc9aa28ccac4f4cb1f4c205294
Gabriela <gdemelo.mendes@gmail.com>
2023-11-15 21:39:21 -0300

    Created menu to contain cabins options

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -1,15 +1,14 @@
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import styled from "styled-components";
-import { HiSquare2Stack, HiTrash, HiPencil } from "react-icons/hi2";
 
-import Modal from "../../ui/Modal";
-import CreateCabinForm from "./CreateCabinForm";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Modal from "../../ui/Modal";
 import Table from "../../ui/Table";
+import CreateCabinForm from "./CreateCabinForm";
 
 import { formatCurrency } from "../../utils/helpers";
-import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin";
-import Menus from "../../ui/Menus";
+import { useDeleteCabin } from "./useDeleteCabin";
 
 const Img = styled.img`
 	display: block;
@@ -75,29 +74,25 @@ function CabinRow({ cabin }) {
 				<span>&mdash;</span>
 			)}
 			<div>
+				<button disabled={isCreating} onClick={handleDuplicate}>
+					<HiSquare2Stack />
+				</button>
+
 				<Modal>
-					<Menus.Menu>
-						<Menus.Toggle id={cabinId} />
-
-						<Menus.List id={cabinId}>
-							<Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
-								Duplicate
-							</Menus.Button>
-
-							<Modal.Open opens="edit">
-								<Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-							</Modal.Open>
-
-							<Modal.Open opens="delete">
-								<Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-							</Modal.Open>
-						</Menus.List>
-					</Menus.Menu>
-
+					<Modal.Open opens="edit">
+						<button>
+							<HiPencil />
+						</button>
+					</Modal.Open>
 					<Modal.Window name="edit">
 						<CreateCabinForm cabinToEdit={cabin} />
 					</Modal.Window>
 
+					<Modal.Open opens="delete">
+						<button>
+							<HiTrash />
+						</button>
+					</Modal.Open>
 					<Modal.Window name="delete">
 						<ConfirmDelete
 							resourceName="cabins"
